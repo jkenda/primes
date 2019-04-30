0a9b96742be554eb5df2bbef5aed9e84bd96794a
KendaJ99 <kenda.jakob@gmail.com>
2019-04-30 12:41:40 +0200

    Začetek dela na I/O funkcionalnosti

--- Prastevila.c
+++ Prastevila.c
@@ -4,116 +4,65 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define ST_NITI 4
-
-int  prastevilo_max, stPrastevil;
-int  *eratosten;
-int  i;
-int  j[ST_NITI];
+int prastevilo_max;
+int *eratosten;
+int stPrastevil;
+int i;
 long time_z;
-int  stDeliteljev;
-int  lock;
 
-pthread_mutex_t mutex  = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t prestej = PTHREAD_COND_INITIALIZER;
-pthread_cond_t nova    = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-// nit za izpisovanje napredka
+// nit za izpisovanje
 void *izpisi(void* param) {
-  int s, h, m;
   while (i < prastevilo_max) {
     sleep(1);
-    s = time(NULL) - time_z;
-    h = s / 3600;
-    s %= 3600;
-    m = s / 60;
-    s %= 60;
-    printf("%d / 1000, %dh %dm %ds\r", (int) ((float) i / prastevilo_max * 1000), h, m, s);
+    printf("%d / 1000, %ld s\r", (int) ((float) i / prastevilo_max * 1000), time(NULL) - time_z);
     fflush(stdout);
   }
-  pthread_exit(0);
   return NULL;
 }
 
-// niti za preverjanje števil
-void *racunaj(void *param) {
-  int *nit = param;
-  int check;
-  while (i <= prastevilo_max) {
-    check = 0;
-    // printf("Thread racunaj: i = %d, j = %ls\r", i, j);
-    // fflush(stdout);
-    if (stDeliteljev != 0) pthread_cond_signal(&prestej);
-    else if (i % eratosten[j[*nit]] == 0) {
-      stDeliteljev += 1;
-    }
-    pthread_mutex_lock(&mutex);
-    j[*nit] += ST_NITI;
-    pthread_mutex_unlock(&mutex);
-    sleep(1);
-    if (j[*nit] >= stPrastevil) pthread_cond_wait(&nova, &mutex);
-    for (int k = 0; k < ST_NITI; k++) if (j[k] >= stPrastevil) check++;
-    if (check == ST_NITI) {
-      // pojdi na preverjanje
-      pthread_cond_signal(&prestej);
-    }
-  }
-  pthread_exit(0);
-  return NULL;
-}
-
-// glavna nit
 int main(int argc, char *argv[]) {
-  // ustvarimo spremeljivke za številke niti
-  int idNiti[ST_NITI];
-  for (int i = 0; i < ST_NITI; i++) idNiti[i] = i;
-  // preberemo argument z največjim številom
-  prastevilo_max = strtol(argv[1], NULL, 10);
-  // dodelimo pomnilnik na kopici
-  eratosten = (int*) malloc(prastevilo_max * sizeof(int));
+  eratosten = (int*) malloc(prastevilo_max/10 * sizeof(int));
   if (eratosten == NULL) {
     printf("Napaka: premalo pomnilnika.\n");
     exit(1);
   }
-  //
-  stPrastevil = ST_NITI;
-  eratosten[0] = 2;
-  eratosten[1] = 3;
-  eratosten[2] = 5;
-  eratosten[3] = 7;
-  i = 8;
-  for (int k = 0; k < ST_NITI; k++) j[k] = k;
-  pthread_t izpisovanje_nit;
-  pthread_t prastevila[ST_NITI];
+  stPrastevil = 0;
+  FILE *f;
+  f = fopen("Prastevila-izpis.txt", "r");
+  printf("Berem ...\r");
+  do {
+    fscanf(f, "%d, ");
+  } while (c != EOF);
+  fclose(f);
+  printf("Končano.\r");
+
+  prastevilo_max = strtol(argv[1], NULL, 10);
+  pthread_t thread;
   time_z = time(NULL);
-  // začni nit za izpisovanje
-  pthread_create(&izpisovanje_nit, NULL, izpisi, NULL);
-  // začni niti za računanje
-  for (int k = 0; k < ST_NITI; k++) pthread_create(&prastevila[k], NULL, racunaj, &idNiti[k]);
-  // pojdi skozi vsa števila
+  pthread_create(&thread, NULL, izpisi, NULL);
+  i = 2;
   while (i <= prastevilo_max) {
-    pthread_cond_wait(&prestej, &mutex);
-    // pthread_mutex_lock(&mutex);
-    if(stDeliteljev == 0) {
+    int stDeliteljev = 0;
+    for (int j = 0; j < stPrastevil; j++) {
+      if (stDeliteljev > 2) break;
+      else if (i % eratosten[j] == 0) stDeliteljev += 1;
+    }
+    if(stDeliteljev < 2) {
       eratosten[stPrastevil] = i;
       stPrastevil += 1;
     }
-    // printf("Thread main: i = %d, j = %ls\r", i, j);
+    pthread_mutex_lock(&mutex);
     i++;
-    stDeliteljev = 0;
-    for (int k = 0; k < ST_NITI; k++) j[k] = k;
-    // pthread_mutex_unlock(&mutex);
-    pthread_cond_broadcast(&nova);
+    pthread_mutex_unlock(&mutex);
   }
-  // počakaj na konec niti
-  pthread_join(izpisovanje_nit, NULL);
-  for (int k = 0; k < ST_NITI; k++) pthread_join(prastevila[k], NULL);
+  pthread_join(thread, NULL);
   printf("Praštevil do %d je %d\nZapisovanje ...\n", prastevilo_max, stPrastevil);
-  // zapiši rezultat v datoteko
-  FILE *f;
+
   f = fopen("Prastevila-izpis.txt", "w");
-  fprintf(f, "Praštevil do %d je %d.\n", prastevilo_max, stPrastevil);
-  for (int i = 0; i < stPrastevil-1; i++) fprintf(f, "%d, ", eratosten[i]);
+  fprintf(f, "Praštevil do %d je _%d.\n", prastevilo_max, stPrastevil);
+  for (int i = 1; i < stPrastevil-1; i++) fprintf(f, "%d, ", eratosten[i]);
   fprintf(f, "%d\n", eratosten[stPrastevil-1]);
   free(eratosten);
   fclose(f);
